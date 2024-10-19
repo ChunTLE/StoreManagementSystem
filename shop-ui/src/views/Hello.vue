7c8d7db76ffdb4e5ce6eefa5c8039b495fbef52d
pcs <peichunshu@163.com>
2024-10-19 22:32:57 +0800

    解决axios跨域问题

---
+++ shop-ui/src/views/Hello.vue
@@ -0,0 +1,16 @@
+<script setup>
+import http from "../util/http.js"
+// import axios from "axios";
+http.get("/list")
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err => { console.log(err) })
+</script>
+<template>
+    <h1>Hello, world!</h1>
+    <h1>Hello, world!</h1>
+    <h1>Hello, world!</h1>
+    <h1>Hello, world!</h1>
+</template>
+<style scoped></style>
